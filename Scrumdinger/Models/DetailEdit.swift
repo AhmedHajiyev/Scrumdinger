19c6d1217cc618d1e10a8847515989df8e530255
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2022-02-21 22:47:04 +0400

    edit detail

---
+++ Scrumdinger/Models/DetailEdit.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEdit.swift
+//  Scrumdinger
+//
+//  Created by Ahmed Hajıyev on 21.02.22.
+//
+
+import SwiftUI
+
+struct DetailEdit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEdit_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEdit()
+    }
+}
