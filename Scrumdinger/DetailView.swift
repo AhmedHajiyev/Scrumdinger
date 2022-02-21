f96c46396d1ec26cd7ac8427af81e91bd236a242
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2022-02-21 22:11:12 +0400

    switch between views

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
-    
+
     var body: some View {
-        List{
+        List {
             Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
                 Label("Start Meeting", systemImage: "timer")
                     .font(.headline)
                     .foregroundColor(.accentColor)
+                }
                 HStack {
-                    Label("Lenght", systemImage: "clock")
+                    Label("Length", systemImage: "clock")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
@@ -26,24 +28,23 @@ struct DetailView: View {
                     Label("Theme", systemImage: "paintpalette")
                     Spacer()
                     Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
                 }
-                
-                .padding(4)
-                .foregroundColor(scrum.theme.accentColor)
-                .background(scrum.theme.mainColor)
-                .cornerRadius(4)
-                
+                .accessibilityElement(children: .combine)
             }
-            .accessibilityElement(children: .combine)
-        }
-        Section(header: Text("Attendees")) {
-            ForEach(scrum.attendees) { attendee in
-                Label(attendee.name, systemImage: "person")
-                
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
+
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
